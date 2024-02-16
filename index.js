06c5975f045cff771c0f214c02b828ababb5b662
Junior_Kamanda <jkamanda062gmail.com>
2024-02-16 21:55:27 +0100

    Gaming Campus version Beta

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const btnHeader = document.querySelector(".btnHeader")
 const imgAnim = document.querySelector("#logo > img")
 const divAnims = document.querySelectorAll(".main-news-container > div");
-const sidebar = document.getElementById("side-bar");
+const listes = document.querySelectorAll(".liste")
 
-lastScroll = scrollY
+let lastScroll = scrollY
+
+window.addEventListener("load", ()=>{
+    btnHeader.classList.add("btnAnim");
+    imgAnim.classList.add("imgAnim");
+    titleHeader.classList.add("titleHeader");
+})
 
 window.addEventListener("scroll", ()=>{
     if (lastScroll > scrollY) {
         nav.style.top = 0;
-        // btn.style.left = "240px";
+        btn.style.top = "10px";
         if (scrollY == 0) {
             nav.style.opacity = 0.88;
         }else{
@@ -16,7 +22,7 @@ window.addEventListener("scroll", ()=>{
         }
     }else{
         nav.style.top = "-70px";
-        // btn.style.left = "-240px";
+        btn.style.top = "-70px";
     }
     lastScroll = scrollY;
 
@@ -27,22 +33,25 @@ window.addEventListener("scroll", ()=>{
     })
 })
 
-window.addEventListener("load", ()=>{
-    btnHeader.classList.add("btnAnim");
-    imgAnim.classList.add("imgAnim");
-    titleHeader.classList.add("titleHeader");
-
-    if (window.innerWidth > 697) {
-        btn.style.display = "none";
-    }else{
-        btn.style.display = "block";
-    }
-})
-
+/* SIDEBAR */
+/* SIDEBAR */
+/* SIDEBAR */
+/* SIDEBAR */
+contents.addEventListener("click", ()=>{
+    sidebar.classList.remove("showsidebar")
+    btn.classList.remove("togglebtn")
+});
 btn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("showSidebar")
-})
+    sidebar.classList.toggle("showsidebar")
+    btn.classList.toggle("togglebtn")
+});
 
-All_content.addEventListener("click", ()=>{
-    sidebar.classList.remove("showSidebar")
-})
+//LesListesDeLaNavbar
+//LesListesDeLaNavbar
+//LesListesDeLaNavbar
+listes.forEach((liste)=>{
+    liste.addEventListener("click", ()=>{
+        document.querySelector(".active")?.classList.remove("active");
+        liste.classList.add("active");
+    });
+});
